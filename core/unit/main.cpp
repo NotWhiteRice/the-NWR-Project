a6d9197e09e3a2b0ab948547167d744535a65769
Tristan Sunico <tristan.sunico@gmail.com>
2023-06-05 14:52:59 -0400

    Made the exe show all the characters that can be printed in console

--- core/unit/main.cpp
+++ core/unit/main.cpp
@@ -7,9 +7,16 @@ import nwr.primitive;
 using namespace nwr;
 
 int main( ) {
-	std::cout << sizeof(char8_t) << std::endl;
-	Character ch(0x265a);
-	std::cout << ch << std::endl;
+	std::cout << "   0123456789ABCDEF" << std::endl;
+	for(int i = 0; i < 16; i++) {
+		std::cout << (char) (i + (i > 9 ? 0x37 : 0x30)) << ": ";
+		for(int j = 0; j < 16; j++) {
+			int v = 16 * i + j;
+			if(v == 8 || v == 9 || v == 10 || v == 13) std::cout << " ";
+			else std::cout << (unsigned char) v;
+		}
+		std::cout << std::endl;
+	}
 	printHeap( );
 	return 0;
 }
